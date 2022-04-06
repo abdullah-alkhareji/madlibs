8434fe93a3f223987cbd20e6d8b627aa5d6745a2
abdullah-alkhareji <outsiderandco@gmail.com>
2022-04-06 11:12:27 +0300

    try again

--- madlibs.py
+++ madlibs.py
@@ -2,11 +2,11 @@ def main():
 	# write your code here
 	time = input("Enter a number from 1 to 12: ")
 	items = input("Enter a noun (plural): ")
-	name = input("Enter a name: ")
-	scream = input("Enter any sentance: ")
+	name = input("Enter a name: ").title()
+	scream = input("Enter any sentance: ").upper()
 	action = input("Enter a verb: ")
 	print(
-		f"It was {time} o'clock when I heard a knock at the door \nI opened the door and there was a box full of {items.capitalize()} with a note saying \"From: {name.capitalize()}\".\nJust as I closed the door I heard a scream '{scream.upper()}'.\nI froze in my place and all I could do was {action.capitalize()}."
+		f"It was {time} o'clock when I heard a knock at the door \nI opened the door and there was a box full of {items} with a note saying \"From: {name}\".\nJust as I closed the door I heard a scream '{scream}'.\nI froze in my place and all I could do was {action}."
 	)
 
 
